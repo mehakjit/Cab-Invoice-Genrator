a56c295708d3d08bc3221459676263f3f38eb90e
Mehakjit <mehakjitsingh@gmail.com>
2020-10-26 07:46:19 +0530

    [Mehakjit: Added Ability to Calculate Fare

---
+++ src/test/java/com/capg/InvoiceServiceTest.java
@@ -0,0 +1,31 @@
+package com.capg;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+	InvoiceGenerator invoiceGenerator;
+
+	@Before
+	public void setUp() {
+		invoiceGenerator = new InvoiceGenerator();
+	}
+
+	@Test
+	public void givenDistanceTimeReturnFare() {
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
+
+	@Test
+	public void givenLessDistanceTimeReturnMinFare() {
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
+
+}
